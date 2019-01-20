7facb7721af4e1e1edeba3d5357dadedcaf718e7
Byron Baldwin <byronsbaldwin@gmail.com>
2019-01-19 23:19:34 -0600

    Updating Progress on Resume

--- app.js
+++ app.js
@@ -6,6 +6,73 @@ function capitalizeMyname(name) {
 
 capitalizeMyname(name);
 
+let myCareer = 'Hotel General Manager';
+
+function toDisplayCareerName(myCareer) {
+    console.log('Career: ' + myCareer);
+}
+
+toDisplayCareerName(myCareer);
+
+let myCareerDescription = 'Train, manage, and lead fifty hourly employees and four department managers';
+
+
+function toDisplayCareerDescription(myCareerDescription) {
+    console.log('Description: ' + myCareerDescription);
+}
+
+toDisplayCareerDescription(myCareerDescription);
+
+let space = '  ';
+
+function displaySpace(space) {
+    console.log('  ' + space);
+}
+
+displaySpace(space);
+
+
+let interests = ['Sports', 'Fishing', 'Dining Out', 'Web Development', ];
+
+function displayMyInterests(interests) {
+    console.log(interests);
+}
+
+displayMyInterests('My Interests:');
+
+displayMyInterests('*' + interests[0]);
+displayMyInterests('*' + interests[1]);
+displayMyInterests('*' + interests[2]);
+displayMyInterests('*' + interests[3]);
+
+
+displaySpace(space);
+
+let experience = 'My Previous Experience:';
+
+function myExperience(experience) {
+    console.log(experience);
+}
+
+myExperience(experience);
+
+let previousExperience = ['General Manager of the Fairfield Inn & Suites Birmingham/Fultondale', 'Assistant General Manager of the Hampton Inn Galleria Parkway Hoover Alabama', 'Guest Service Representative at the Hampton Inn Galleria Parkway Hoover Alabama', 'Night Auditor at the Holiday Inn Express Inverness Birmingham Alabama'];
+
+function myPreviousExperience(previousExperience) {
+    console.log(previousExperience);
+}
+
+myPreviousExperience('*' + previousExperience[0]);
+myPreviousExperience('*' + previousExperience[1]);
+myPreviousExperience('*' + previousExperience[2]);
+myPreviousExperience('*' + previousExperience[3]);
+
+
+
+
+
+
+
 
 
 
