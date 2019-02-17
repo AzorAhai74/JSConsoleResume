d0882b01e803e374c02d39a5b8f727b3acb5a570
Byron Baldwin <byronsbaldwin@gmail.com>
2019-02-16 20:56:07 -0600

    Completed Javascript Resume

--- app.js
+++ app.js
@@ -40,10 +40,10 @@ function displayMyInterests(interests) {
 
 displayMyInterests('My Interests:');
 
-displayMyInterests('*' + interests[0]);
-displayMyInterests('*' + interests[1]);
-displayMyInterests('*' + interests[2]);
-displayMyInterests('*' + interests[3]);
+displayMyInterests(' * ' + interests[0]);
+displayMyInterests(' * ' + interests[1]);
+displayMyInterests(' * ' + interests[2]);
+displayMyInterests(' * ' + interests[3]);
 
 
 displaySpace(space);
@@ -62,10 +62,79 @@ function myPreviousExperience(previousExperience) {
     console.log(previousExperience);
 }
 
-myPreviousExperience('*' + previousExperience[0]);
-myPreviousExperience('*' + previousExperience[1]);
-myPreviousExperience('*' + previousExperience[2]);
-myPreviousExperience('*' + previousExperience[3]);
+myPreviousExperience(' * ' + previousExperience[0]);
+myPreviousExperience(' * ' + previousExperience[1]);
+myPreviousExperience(' * ' + previousExperience[2]);
+myPreviousExperience(' * ' + previousExperience[3]);
+
+displaySpace(space);
+
+
+let mySkills = 'My Skills:';
+
+
+console.log(mySkills);
+
+let skill = ['Leadership', 'Public Speaking', 'Revenue Management', 'Javascript', 'Data Analytics', 'Payroll', 'HTML', 'CSS', 'Data Analytics'];
+
+
+function displayMySkills(skill, cool) {
+    if (cool === true) {
+        console.log(' * BAM!:' + skill);
+    } else {
+        console.log(' * ' + skill);
+    }
+}
+
+
+displayMySkills('Leadership', true);
+displayMySkills('Public Speaking');
+displayMySkills('Revenue Management', true);
+displayMySkills('Javascript', true);
+displayMySkills('Data Analytics', true);
+displayMySkills('Payroll');
+displayMySkills('HTML');
+displayMySkills('CSS');
+
+
+
+
+displaySpace(space);
+
+let bsbPositions = ' My Positions:';
+
+function myPositions(bsbPositions) {
+    console.log(bsbPositions);
+}
+
+myPositions(bsbPositions);
+
+
+let companyName = ['Homewood Suites by Hilton Near U.A.B.', 'Fairfield Inn & Suites Birmingham/Fultondale',  'Hampton Inn & Suites Galleria Parkway, Hoover', 'At&t'];
+
+let jobTitle = ['General Manager', 'Assistant General Manager', 'Sales Representative'];
+
+let description = ['Recruit, hire, train, and lead fifty hourly employees and four department managers for a 105 room extended stay hotel',
+                    'Recruited, hired, and trained twenty-five hourly employees for a 75 room hotel', 
+                    'Assisted the General Manager in training front desk and breakfast employees on Hilton standards, employee scheduling, and customer satisfaction',
+                    'Sold mobile handset devices, data plans, accessories, and handset upgrades'];
+
+ function displayPosition(companyName, jobTitle, description) {
+     console.log(' * Company Name: ' + companyName + '\n' + ' * Job Title:', jobTitle + '\n' + ' * Job Description:', description);
+ }
+
+displayPosition(companyName[0], jobTitle[0], description[0]);
+
+displayPosition(companyName[1], jobTitle[0], description[1]);
+
+displayPosition(companyName[2], jobTitle[1], description[2]);
+
+displayPosition(companyName[3], jobTitle[2], description[3]);
+
+
+
+
+
 
 
 
